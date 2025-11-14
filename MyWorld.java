ff395b914da0eb3e609eb14856860d6201a0e410
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-11-14 14:35:07 -0500

    added Apple and Elephant

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,16 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 100);
+        
+        spawnApple();
+    }
+    
+    public void spawnApple()
+    {
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
