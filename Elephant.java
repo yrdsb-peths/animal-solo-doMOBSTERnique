5cc4bbe155245581dc8537df130f4da2bf9a6c80
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-11-25 14:41:33 -0500

    animated elephant idle

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephant-growl.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant()
+    {
+        for (int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     
     public void act()
     {
@@ -23,8 +41,7 @@ public class Elephant extends Actor
         
         eat();
         
-        {
-        }
+        animateElephant();
     }
     
     public void eat()
